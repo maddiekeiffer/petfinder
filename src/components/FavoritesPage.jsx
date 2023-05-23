f26ce93291778f022e500fca66cf0a2dd1d2289b
maddiekeiffer <madisonkeiffer@gmail.com>
2023-05-22 19:54:32 -0500

    Updated FavoritesPage

--- src/components/FavoritesPage.jsx
+++ src/components/FavoritesPage.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import AnimalDisplay from './AnimalDisplay';
+import { useFavoritesContext } from '../context/FavoritesContext';
 
 function FavoritesPage() {
+  const {favorites, removeFavorite} = useFavoritesContext();
+
   return (
-    <div>FavoritesPage</div>
+    <div>
+    <h2>Favorites:</h2>
+    {favorites.map((val) => {
+      return(
+        <AnimalDisplay
+          url={val.url}
+          key={val.pet_id}
+          pet={val.pet}
+          pet_id={val.pet_id}
+          image={val.image}
+          name={val.name}
+          breeds={val.breeds}
+          age={val.age}
+          gender={val.gender}
+          distance={val.distance}
+          isFavorite={true}
+          removeFavorite={removeFavorite}
+          />
+      );
+    })}
+    </div>
   )
 }
 
