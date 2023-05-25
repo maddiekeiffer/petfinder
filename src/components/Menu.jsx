367b87244e67fdcc4bd0b4b1f63d2a137cdb4cdd
maddiekeiffer <madisonkeiffer@gmail.com>
2023-05-24 20:34:11 -0500

    Updated functionality

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { React }from 'react';
 import { Box, Tabs, Tab } from '@mui/material';
 import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
 import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
@@ -8,75 +7,75 @@ import { styled } from '@mui/material/styles';
 import { useUserContext } from '../context/UserContext';
 import { useSearchContext } from '../context/SearchContext';
 import { useFavoritesContext } from '../context/FavoritesContext';
-
-const StyledTab = styled((props) => <Tab disableRipple {...props} />)(
-  ({
-    marginRight: '5px',
-    color: 'rgba(64, 64, 64, 0.7)',
-    '&.Mui-selected': {
-      color: '#fff',
-    },
-  }),
-);
-const StyledTabs = styled((props) => (
-  <Tabs
-    {...props}
-    TabIndicatorProps={{ children: <span className="MuiTabs-indicatorSpan" /> }}
-      />
-    ))({
-      '& .MuiTabs-indicator': {
-        display: 'flex',
-        justifyContent: 'center',
-        backgroundColor: 'transparent',
-        marginBottom: '5px',
-      },
-      '& .MuiTabs-indicatorSpan': {
-        width: '100%',
-        backgroundColor: '#fff',
-      },
-});
+import { useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Menu() {
-  const { clearUser } = useUserContext();
-  const { clearFavorites } = useFavoritesContext();
-  const { clearSearchResults } = useSearchContext();
+    const { clearUser } = useUserContext();
+    const { clearFavorites } = useFavoritesContext();
+    const { clearSearchResults } = useSearchContext();
 
-  const location = useLocation();
-  let value;
+    const location = useLocation();
+    let value;
 
-  switch (location.pathname) {
-    case '/login':
-        value = 'login';
-        break;
-    case '/search':
-        value = 'search';
-        break;
-    case '/favorites':
-        value = 'favorites';
-        break;
-    default:
-        value = false;
-        break;
-  }
+    switch (location.pathname) {
+        case '/login':
+            value = '/login';
+            break;
+        case '/search':
+            value = '/search';
+            break;
+        case '/favorites':
+            value = '/favorites';
+            break;
+        default:
+            value = null;
+            break;
+    }
 
-    
-  return (
-    <Box sx={{ width: '100%', bgcolor: 'rgba(153, 51, 255, 0.7)'}}>
-        <StyledTabs value={value} centered>
-            <StyledTab icon={<LogoutRoundedIcon />} value='login' label='Logout' href='/login' 
-            onClick={() => {
-            clearUser();
-            clearFavorites();
-            clearSearchResults();
-            }}  
+    const handleLogout = () => {
+        clearUser();
+        clearFavorites();
+        clearSearchResults();
+      };
+
+      const StyledTab = styled((props) => <Tab disableRipple {...props} />)(
+        ({
+          marginRight: '5px',
+          color: 'rgba(64, 64, 64, 0.7)',
+          '&.Mui-selected': {
+            color: '#fff',
+          },
+        }),
+      );
+      const StyledTabs = styled((props) => (
+        <Tabs
+          {...props}
+          TabIndicatorProps={{ children: <span className="MuiTabs-indicatorSpan" /> }}
             />
-            <StyledTab icon={<SearchRoundedIcon />}
-            value='search' label='Search' href='/search' />
-            <StyledTab icon={<FavoriteRoundedIcon />}
-            value='favorites' label='Favorites' href='/favorites' />
-        </StyledTabs>
-    </Box> 
-  )
+          ))({
+            '& .MuiTabs-indicator': {
+              display: 'flex',
+              justifyContent: 'center',
+              backgroundColor: 'transparent',
+              marginBottom: '5px',
+            },
+            '& .MuiTabs-indicatorSpan': {
+              width: '100%',
+              backgroundColor: '#fff',
+            },
+      });
+
+
+return (
+  <Box sx={{ width: '100%', bgcolor: 'rgba(153, 51, 255, 0.7)'}}>
+      <StyledTabs value={value || false} centered>
+          <StyledTab component={Link} to="/login" icon={<LogoutRoundedIcon />} value='/login' label='Logout' onClick={handleLogout} />
+          <StyledTab component={Link} to="/search" icon={<SearchRoundedIcon />} value='/search' label='Search' />
+          <StyledTab component={Link} to="/favorites" icon={<FavoriteRoundedIcon />} value='/favorites' label='Favorites' />
+      </StyledTabs>
+  </Box> 
+)
 }
 
 export default Menu;
